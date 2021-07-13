04f119ff89a094d063f4e3ccb95ff99130cd96ee
Nicholas Young <mrbiggbraiN@gmail.com>
2021-07-12 22:34:43 -0400

    Converted the deck code to use a liquidjs template instead of the complex functions I was using.

--- .eleventy.js
+++ .eleventy.js
@@ -16,22 +16,10 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPairedLiquidShortcode("card", shortcode_card);
 
-  /* Testing new deck builder */
-  eleventyConfig.addPairedLiquidShortcode("test", shortcode_test);
-
   /* Passthrough Files */
   eleventyConfig.addPassthroughCopy("js");
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
-}
-
-async function shortcode_deck(content, deckName)
-{
-  // Parse the content of the tag to get a functioning deck.
-  var deck = await ParseDeck(content);
-
-  // Render and return the HTML making up the deck area.
-  return RenderDeckHTML(deckName, deck);
 }
 
 async function shortcode_card(content)
@@ -42,123 +30,27 @@ async function shortcode_card(content)
 
 }
 
-async function shortcode_test(content, deckname)
+async function shortcode_deck(content, deckname)
 {
   // Parse the content of the tag to get a functioning deck.
   var deck = await ParseDeck(content);
+  var wildcards = GetWildcards(deck);
 
-  var template = fs.readFileSync("./_includes/deck.liquid").toString('utf-8');
+  var template = fs.readFileSync("./_includes/deck.html").toString('utf-8');
 
-  return engine.parseAndRender(template, deck);
-}
+  var gates = { commander: deck.zones.commander.length > 0, 
+                companion: deck.zones.companion.length > 0,
+                deck: deck.zones.deck.length > 0,
+                sideboard: deck.zones.sideboard.length > 0
+              };
 
-// ## Rendering HTML ## //
+  console.log(gates);
 
-/* Render the Deck to HTML */
-function RenderDeckHTML(deckName, deck)
-{
-  // Use cheerio
-  const $ = cheerio.load('', null, false)
+  var html = await engine.parseAndRender(template, {deck: deck, wildcards: wildcards, name: deckname, gates: gates });
 
-  let commander_outline = GenerateOutline("Commander", deck.zones.commander, deck.id);
-  let companion_outline = GenerateOutline("Companion", deck.zones.companion, deck.id);
-  let deck_outline = GenerateOutline("Deck", deck.zones.deck, deck.id)
-  let sideboard_outline = GenerateOutline("Sideboard", deck.zones.sideboard, deck.id);
-  let wildcard_outline = GenerateWildcardOutline(deck);
+  console.log(Outdent(html));
 
-  let links_col = `<div class="container">${commander_outline} ${companion_outline} ${deck_outline} ${sideboard_outline} ${wildcard_outline}</div>`;
-
-  let image_col = `<image class="img-fluid" src="/img/Magic_card_back.jpg" data-deck-name="${deck.id}" />`;
-
-
-  let left_root_cols = `<div class="col-4">${image_col}</div><div class="col-8">${links_col}</div>`;
-  let left_root_rows = `<div class="row">${left_root_cols}</row>`;
-  let left_root = `<div class="container">${left_root_rows}</div>`;
-  var left = $(`<div class="col-sm-8 text-center" style="padding-right:20px; border-right: 1px solid #ccc;"><h3>${deckName}</h3><hr />${left_root}</div>`);
-
-
-  /*
-  // Build the right raw view
-  */
-  var right = $(`<div class="col-sm-4 text-center">`);
-  right.append(`<h3>Raw Text</h3>`);
-  right.append(`<hr />`);
-  right.append(`<pre>${deck.raw}</pre>`);
-
-
-  /*
-  // Combine them.
-  */
-
-  /* Create a ruler item */
-  var ruler_12 = $(`<div class="row"><div class="col-sm-12 text-center"><hr /></div></div>`); /* A wide line for display */
-  
-  /* Combine left and right into mid */
-  var mid = $(`<div class="row"></div>`);
-  mid.append(left);
-  mid.append(right);
-
-  /* Add the rules and the mid content */
-  var top = $(`<div class="container"></div>`); /* Topmost container */
-  top.append(`<div class="row"><div class="col-sm-12 text-center"><hr /></div></div>`);
-  top.append(mid);
-  top.append(`<div class="row"><div class="col-sm-12 text-center"><hr /></div></div>`);
-
-  return top.html();
-  
-}
-
-function GenerateOutline(name, cards, id)
-{
-  var outline = ``;
-
-  if(cards.length > 0)
-  {
-    outline += `<div class="row"><div class="col-12 py-0"><b>${name} (${CardsToCount(cards)})</b><hr /></div></div>`;
-    
-    let half_a = ``;
-    let half_b = ``;
-
-    cards.forEach(function (card, i){
-      let s = `${card.count} <a  href="${card.details.scryfall_uri}" data-deck-name="${id}" data-card-rarity=${card.details.rarity} data-image-url="${card.details.image_uris.normal}" target="_blank" rel="noopener noreferrer">${card.details.name}</a><br />`;
-      if(i > (cards.length / 2)) half_b += s;
-      else half_a += s;
-    });
-
-    outline += `<div class="row"><div class="col-6 text-start">${half_a}</div><div class="col-6 text-start">${half_b}</div></div>`;
-  }
-
-  return outline;
-}
-
-function GenerateWildcardOutline(deck)
-{
-  let wildcards = GetWildcards(deck);
-
-  let mythic = GenerateWildcardData(wildcards.mythic, "orange", "Mythic", deck.id);
-  let rare = GenerateWildcardData(wildcards.rare, "gold", "Rare", deck.id);
-  let uncommon = GenerateWildcardData(wildcards.uncommon, "silver", "Uncommon", deck.id);
-  let common = GenerateWildcardData(wildcards.common, "black", "Common", deck.id);
-
-  let outline = `<div class="row"><div class="col-12 py-0"><hr /></div></div>`;
-  outline += `<div class="row">`;
-  outline += `<div class="col-3">${mythic}</div>`;
-  outline += `<div class="col-3">${rare}</div>`;
-  outline += `<div class="col-3">${uncommon}</div>`;
-  outline += `<div class="col-3">${common}</div>`;
-  outline += `</div>`
-
-  return outline;
-}
-
-function GenerateWildcardData(count, color, alt, deckId)
-{
-  return `<div data-bs-toggle="tooltip" title="${alt}" data-card-rarity="${alt.toLowerCase()}" data-deck-name="${deckId}"> ${WildcardIcon(color)}</div> <b>${count}</b>`
-}
-
-function WildcardIcon(color, alt)
-{
-  return`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="${color}" class="bi bi-heptagon-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M7.779.052a.5.5 0 0 1 .442 0l6.015 2.97a.5.5 0 0 1 .267.34l1.485 6.676a.5.5 0 0 1-.093.415l-4.162 5.354a.5.5 0 0 1-.395.193H4.662a.5.5 0 0 1-.395-.193L.105 10.453a.5.5 0 0 1-.093-.415l1.485-6.676a.5.5 0 0 1 .267-.34L7.779.053z"/></svg>`;
+  return Outdent(html);
 }
 
 // ## Parsing Data ## //
@@ -377,4 +269,20 @@ function ParseLines(content)
 {
   /* Grab any lines with content on them */
   return content.match(/.+/g);
+}
+
+// Remove extra spaces in HTML
+function Outdent(aString)
+{
+  let parts = aString.split('\n');
+
+  let final = ``;
+
+  for(var line of parts)
+  {
+    final += `${line.trim()}\n`
+  }
+
+  return final;
+
 }
